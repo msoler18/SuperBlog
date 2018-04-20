fd00986e10b567c34aea8bb37832a042b1918987
msoler18 <msoler735@gmail.com>
2018-04-20 16:54:00 -0500

    Add validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,9 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :comments
+
+  validates_associated :user
+  validates :title, presence: true
+  validates :content, presence: true
+  validates :content, length: {minimum: 250}
 end
